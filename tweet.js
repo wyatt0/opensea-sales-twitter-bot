0875afa9b318c1cd6862fa98af79febbe1165a2f
wyatt0 <48571570+wyatt0@users.noreply.github.com>
2021-08-09 16:50:38 -0500

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -104,7 +104,7 @@ async function tweet(tweetText, imageUrl) {
 // Format a provided URL into it's base64 representation
 function getBase64(url) {
     console.log("here3");
-    return axios.get(url, { responseType: 'arraybuffer'}).then(response => Buffer.from(response.data, 'binary')
+    return axios.get(url, { responseType: 'arraybuffer'}).then(response => Buffer.from(response.data, 'binary'))
 }
 
 module.exports = {
