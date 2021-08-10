cdeab50ab354412c02429c9f551c236ac8fd865c
wyatt0 <48571570+wyatt0@users.noreply.github.com>
2021-08-09 19:01:43 -0500

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -67,6 +67,7 @@ async function _arrayBufferToBase64(buffer) {
     }
     return window.btoa( binary );
 }
+
 // Upload image of item retrieved from OpenSea & then tweet that image + provided text
 async function tweet(tweetText, imageUrl) { 
     const prcTest = await getBase64('https://upload.wikimedia.org/wikipedia/commons/4/47/VU-Banana-1000x1000.png');
@@ -84,7 +85,8 @@ async function tweet(tweetText, imageUrl) {
         .then(data => { imagee = data })
         .catch(err => { console.log("Sharp error: " + err) });
     console.log(imagee)
-    const procb64 = _arrayBufferToBase64(imagee);
+    //const procbb64 = Buffer.from(imagee).toString('base64')
+    const procb64 = base64ArrayBuffer(imagee);
                        
     console.log("yoyo")
     //const processedPng = await getBase64(pngimageUrl);
@@ -118,7 +120,58 @@ function getBase64(url) {
     console.log("here3");
     return axios.get(url, { responseType: 'arraybuffer'}).then(response => Buffer.from(response.data, 'binary'))
 }
+function base64ArrayBuffer(arrayBuffer) {
+  var base64    = ''
+  var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 
+  var bytes         = new Uint8Array(arrayBuffer)
+  var byteLength    = bytes.byteLength
+  var byteRemainder = byteLength % 3
+  var mainLength    = byteLength - byteRemainder
+
+  var a, b, c, d
+  var chunk
+
+  // Main loop deals with bytes in chunks of 3
+  for (var i = 0; i < mainLength; i = i + 3) {
+    // Combine the three bytes into a single integer
+    chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+
+    // Use bitmasks to extract 6-bit segments from the triplet
+    a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
+    b = (chunk & 258048)   >> 12 // 258048   = (2^6 - 1) << 12
+    c = (chunk & 4032)     >>  6 // 4032     = (2^6 - 1) << 6
+    d = chunk & 63               // 63       = 2^6 - 1
+
+    // Convert the raw binary segments to the appropriate ASCII encoding
+    base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+  }
+
+  // Deal with the remaining bytes and padding
+  if (byteRemainder == 1) {
+    chunk = bytes[mainLength]
+
+    a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+
+    // Set the 4 least significant bits to zero
+    b = (chunk & 3)   << 4 // 3   = 2^2 - 1
+
+    base64 += encodings[a] + encodings[b] + '=='
+  } else if (byteRemainder == 2) {
+    chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
+
+    a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
+    b = (chunk & 1008)  >>  4 // 1008  = (2^6 - 1) << 4
+
+    // Set the 2 least significant bits to zero
+    c = (chunk & 15)    <<  2 // 15    = 2^4 - 1
+
+    base64 += encodings[a] + encodings[b] + encodings[c] + '='
+  }
+  
+  return base64
+}
 module.exports = {
     handleDupesAndTweet: handleDupesAndTweet
 };
+
